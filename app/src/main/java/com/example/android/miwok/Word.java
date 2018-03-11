955b27d23cb21729415d6a73387627b19845f5a7
mgroesink <mroesink@rocvantwente.nl>
2018-03-11 13:13:11 +0100

    Activities updated

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,67 @@
+package com.example.android.miwok;
+
+/**
+ * Created by Marcel on 11-3-2018.
+ */
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation, a Miwok translation, and an image for that word.
+ */
+public class Word {
+
+    /** Default translation for the word */
+    private String mDefaultTranslation;
+
+    /** Miwok translation for the word */
+    private String mMiwokTranslation;
+
+    /** Image resource ID for the word */
+    private int mImageResourceId;
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+
+    /**
+     *
+     * @param mDefaultTranslation is the word in a language that the user is already familiar with
+     * @param mMiwokTranslation is the word in the Miwok language
+     * @param mImageResourceId
+     */
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int mImageResourceId) {
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+        this.mImageResourceId = mImageResourceId;
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    /**
+     * Return the image resource ID of the word.
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+}
